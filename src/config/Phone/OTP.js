c3e678a18168e23b05c81e957d9193066a61c943
Priyom Saha <priyom1499@gmail.com>
2021-06-22 10:17:13 +0530

    TEST - TO check backspace function - Mobile 2

--- src/config/Phone/OTP.js
+++ src/config/Phone/OTP.js
@@ -9,22 +9,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function delay(callback, ms) {
-  var timer = 0;
-  return function () {
-    var context = this,
-      args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      callback.apply(context, args);
-    }, ms || 0);
-  };
-}
-
 function OTP(isdisabled) {
   const classes = useStyles();
   const [error, setError] = useState(false);
-  const [counter, setCounter] = useState(0);
 
   const [otp, setotp] = useState({
     otp1: "",
